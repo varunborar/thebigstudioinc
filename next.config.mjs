79e00de794f0cfc81d262877b46e9135a3f2a6db
varunborar <varunborar@gmail.com>
2025-11-06 00:11:41 +0530

    update workflow to deploy to gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = !!process.env.GITHUB_ACTIONS;
+
+let basePath = undefined;
+let assetPrefix = undefined;
+
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: { unoptimized: true },
+  basePath,
+  assetPrefix,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath || "",
+  },
 };
 
 export default nextConfig;
