6d2d3dfcd7558ad4edd6d26a9ab8ef3618d1bb31
varunborar <varunborar@gmail.com>
2025-11-06 00:34:24 +0530

    add buttons

--- src/components/pages/home/landing.jsx
+++ src/components/pages/home/landing.jsx
@@ -10,7 +10,7 @@ export function Landing() {
                 </h1>
             </div>
             <div className="flex flex-row justify-center items-center mt-20">
-                <Button variant="primary" size="lg" className="bg-gray-200 mt-8 hover:bg-gray-300">
+                <Button variant="primary" size="lg" className="bg-gray-200 text-gray-800 mt-8 hover:bg-gray-300">
                     Get Started
                 </Button>
                 <Button variant="secondary" size="lg" className="bg-blue-600 text-gray-200 mt-8 ml-4 hover:bg-blue-700">
